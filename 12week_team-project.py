546ec4e87f90095dd7cf4ae27cf54b09c21388c5
rltjr4362 <dhffls852@naver.com>
2021-05-15 18:59:56 +0900

    'Crawling_2'

--- 12week_team-project.py
+++ 12week_team-project.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 
-url = 'https://www.op.gg/statistics/champion/'
+url = 'https://www.op.gg//'
 
 driver = webdriver.Chrome('chromedriver')
 driver.get(url)
 driver.implicitly_wait(3)
+
+driver.find_element_by_xpath('/html/body/div[2]/div[1]/ul/li[5]/a').click()
+
+data = driver.page_source
 
 driver.quit()
